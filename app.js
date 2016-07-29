ea880c2b5d6b407bec7a198599a5450227d72549
HadockHanif <hanifboss21@yahoo.com>
2016-07-30 02:30:08 +0600

    allow cors request

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/hello', function (request, response, next) {
   console.log('hello world');
