9ab3646a1013baad52472f0a175b1b6783753e3c
hanif <hanif@thetigerworks.com>
2016-08-03 23:44:32 +0600

    added body parser as enable to read sent data from Front

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.get('/hello', function (request, response, next) {
   console.log('hello world');
+  response.send("I'm hanifa");
+});
+app.post('/hello', function (request, response) {
+  console.log('hello world', request.body);
   response.send("I'm hanifa");
 });
 app.get('/users', function (request, response, next) {
